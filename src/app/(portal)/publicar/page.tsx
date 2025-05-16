9ebc9a853d7c8c0bb0a56241a07b724c19827b9e
unknown <daylersan@gmail.com>
2025-05-15 20:55:34 -0500

    feat: add como funciona

---
+++ src/app/(portal)/publicar/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="loader"></div>
+      <h1>Página no disponible por el momento</h1>
+      <p>Por favor, vuelve más tarde.</p>
+    </div>
+  )
+}
